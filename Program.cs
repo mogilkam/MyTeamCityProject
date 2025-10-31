912a2ffe3a1cf217440f62a96a940d16d0ebf6a4
mariia <mariamosik@gmail.com>
2025-10-31 11:09:57 +0200

    Second commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace MyTeamCityProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, TeamCity!");
+            Console.WriteLine("It is project MyTeamCityProject");
+
         }
     }
 }
+
